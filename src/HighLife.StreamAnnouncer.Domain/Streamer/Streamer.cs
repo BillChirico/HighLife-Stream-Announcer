f7687c1b6df0c2ebc66bc08cec397f773339c1e5
billchirico <billchirico@gmail.com>
2020-04-29 23:18:01 -0400

    Add data project Add Streamer model

---
+++ src/HighLife.StreamAnnouncer.Domain/Streamer/Streamer.cs
@@ -0,0 +1,11 @@
+namespace HighLife.StreamAnnouncer.Domain.Streamer
+{
+    public class Streamer
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string TagLine { get; set; }
+    }
+}
